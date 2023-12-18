2dd7cdd9f364f06e7c8133cd24a6f7f84798635f
Joseortiz0918 <145424169+Joseortiz0918@users.noreply.github.com>
2023-12-17 19:35:46 -0600

    added search city function, added submit form function, added event listener on click for search form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,65 @@
-var apiKey = "3723571894b255f187674886d84d9da2"
-var requestCurrent = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=" + apiKey
-var requestWeather = "https://api.openweathermap.org/data/2.5/forecast?units=imperial&appid=" + apiKey;
-var requestLocation = "https://api.openweathermap.org/geo/1.0/direct?limit=1&appid=" + apiKey + "&";
+var apiKey = "4f0caa1e03b1114f7e2701c7497f9f55";
+var requestCurrent =
+  "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=" +
+  apiKey;
+var requestWeather =
+  "https://api.openweathermap.org/data/2.5/forecast?units=imperial&appid=" +
+  apiKey;
+var requestLocation =
+  "http://api.openweathermap.org/geo/1.0/direct?&limit=5&appid="+ apiKey +'&q=';
+
+var searchBtn = document.getElementById("searchBtn");
+var searchText = document.getElementById("searchText");
+var forecast = document.getElementById("forecast");
+var cityName = document.getElementById("cityName");
+var date = document.getElementById("date");
+var temp = document.getElementById("temp");
+var wind = document.getElementById("wind");
+var humidity = document.getElementById("humidity");
+var fiveDayForecast = document.getElementById("5DayForecast");
+
+function searchCity(city){
+  var requestCity= requestLocation + city
+  fetch(requestCity)
+  .then(function(response){
+    return response.json()
+  
+  })
+  .then(function(data){
+    var lon= data[0].lon
+    var lat= data[0].lat
+    console.log(lon)
+    console.log(lat)
+    searchForecast(lon,lat)
+  })
+
+  
+}
+
+function searchForecast(lon,lat){
+  var requestCity = requestCurrent + lon + lat
+  
 
 
-var searchBtn =document.getElementById("searchBtn");
-var searchText =document.getElementById("searchText");
-var forecast =document.getElementById("forecast");
-var cityName =document.getElementById("cityName");
-var date =document.getElementById("date");
-var temp =document.getElementById("temp");
-var wind =document.getElementById("wind");
-var humidity =document.getElementById("humidity");
-var fiveDayForecast =document.getElementById("5DayForecast");
+}
 
 
-function (param) {  }
+// function to make search form equal search text on click. if nothing is in search text error will pop up
+function formSubmitHandler(event){
+ event.preventDefault();
+ var city = searchText.value.trim();
+ if (city){
+   console.log(city)
+   searchCity(city);
+   
+ }
+ else{
+  alert("please enter a valid city name")
+ }
+
+
+}
+
+
+
+searchBtn.addEventListener("click",formSubmitHandler)
