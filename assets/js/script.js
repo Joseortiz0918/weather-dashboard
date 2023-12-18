f1d2888e37967f00040f3229fc8831d3ee6a4c2b
Joseortiz0918 <145424169+Joseortiz0918@users.noreply.github.com>
2023-12-17 22:54:07 -0600

    appended weather forecast for day to html, added icons for weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 var apiKey = "&appid=4f0caa1e03b1114f7e2701c7497f9f55";
 var requestCurrent =
-  "https://api.openweathermap.org/data/2.5/forecast?"+
+  "https://api.openweathermap.org/data/2.5/weather?units=imperial"+
   apiKey;
 var requestWeather =
   "https://api.openweathermap.org/data/2.5/forecast?units=imperial" +
@@ -11,12 +11,14 @@ var requestLocation =
 var searchBtn = document.getElementById("searchBtn");
 var searchText = document.getElementById("searchText");
 var forecast = document.getElementById("forecast");
-var cityName = document.getElementById("cityName");
-var date = document.getElementById("date");
-var temp = document.getElementById("temp");
-var wind = document.getElementById("wind");
-var humidity = document.getElementById("humidity");
+var cityNameEl = document.getElementById("cityName");
+var dateEl = document.getElementById("date");
+var tempEl = document.getElementById("temp");
+var windEl = document.getElementById("wind");
+var humidityEl = document.getElementById("humidity");
 var fiveDayForecast = document.getElementById("5DayForecast");
+var descriptionEl= document.getElementById("description");
+var iconEl=document.getElementById("icon")
 
 function searchCity(city){
   var requestCity= requestLocation + city
@@ -30,13 +32,13 @@ function searchCity(city){
     var lat= data[0].lat
     console.log(lon)
     console.log(lat)
-    searchForecast(lon,lat)
+    todayForecast(lon,lat)
   })
 
   
 }
 
-function searchForecast(lon,lat){
+function todayForecast(lon,lat){
   var requestCity = requestCurrent + "&lon="+lon + "&lat="+lat
   fetch(requestCity)
   .then(function(response){
@@ -44,7 +46,40 @@ function searchForecast(lon,lat){
   })
   .then(function(data){
     console.log(data)
+    
+    var feelsLike =data.main.feels_like
+    var name=data.name
+    var humidity =data.main.humidity
+    var wind= data.wind.speed
+    var temp= data.main.temp
+    var description = data.weather[0].description
+    var iconId=data.weather[0].icon
+    console.log(iconId)
+    propagateForecast(name,feelsLike,humidity,wind,temp,description,iconId)
   })
+
+}
+
+function day () {
+  //sets current date and time 
+  var currentTime = dayjs().format("hh:mm a");
+  var currentDay = dayjs().format("MMM DD YYYY");
+  dateEl.append(currentDay)
+  timeEl.append(currentTime)
+
+}
+day()
+
+
+function propagateForecast(name,feelsLike,humidity,wind,temp,description,iconId){
+   
+  iconEl.setAttribute("src","https://openweathermap.org/img/wn/"+ iconId+"@2x.png")
+  cityNameEl.append(name)
+  humidityEl.append(humidity+"%")
+  windEl.append(wind+"MPH")
+  tempEl.append(temp+"°F")
+  descriptionEl.append(description)
+  console.log(feelsLike)
 
 }
 
